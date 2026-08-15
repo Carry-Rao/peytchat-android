fcd980baf9559581b158a3860f2e34a52b7fb21f
MaherJon <mh13082714815@outlook.com>
2026-08-15 19:04:24 +0800

    feat: 支持了UI通过热更新功能进行更新

---
+++ libs/patch-api/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "cn.yzjtiantian.android.patchapi"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":data"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation("androidx.compose.runtime:runtime")
+    implementation(libs.androidx.core.ktx)
+}
